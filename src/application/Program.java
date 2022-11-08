ea9dd65889612692da4bf27c48e713cdd2113a93
Rafael de Luca <deluca1712@gmail.com>
2022-11-08 10:22:40 -0300

    Conceitos de matrizes

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nMatrizes\n"
+				+ "\tSão arranjos Bidimensionais. É um vetor de vetores.\n"
+				+ "Array\n"
+				+ "\tEstrutura de dados homogênea.\n"
+				+ "\tOrdenada (elementos acessados direto pelas suas posições\n"
+				+ "\tAlocada de uma vez só e um bloco contíguo de memória.\n"
+				+ "\tTem as mesmas vantagens e desvantegens de vetor.\n"
+				+ "Vantagens: acesso imediato aos elementos pela sua posição.\n"
+				+ "Desvantagens: tamanho fixo e dificuldades para realizar insert e delete)");
+
+	}
+
+}
